f55fbab8e3695f8f27c6724b6c454fdd17b47563
singhv1shal <singhvishal0304@gmail.com>
2019-07-08 16:08:55 +0530

    Modified views.py

--- Railways_Blog/views.py
+++ Railways_Blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+from django.contrib.auth.models import User
 from django.utils import timezone
 # Create your views here.
 def post_list(request):
 	#filtering posts in sorted order of published date
-	posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	#posts is used by post_list.html
-	return render(request, 'Railways_Blog/post_list.html',{'Posts':posts})
+	return render(request, 'Railways_Blog/post_list.html',{'posts':posts})
